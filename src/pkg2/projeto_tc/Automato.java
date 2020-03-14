690f625f52203d07dd9a465edb697f7b41a1157c
Victor01Lima <51930046+Victor01Lima@users.noreply.github.com>
2020-03-14 12:13:39 -0300

    Add files via upload

---
+++ src/pkg2/projeto_tc/Automato.java
@@ -0,0 +1,56 @@
+package pkg2.projeto_tc;
+
+import pkg2.projeto_tc.EstadoFinal;
+import pkg2.projeto_tc.EstadoInicial;
+import pkg2.projeto_tc.Estados;
+import pkg2.projeto_tc.Transicao;
+import java.util.ArrayList;
+import java.util.List;
+public class Automato {
+
+    private List<Transicao> transition = new ArrayList();
+    private List<Estados> estado = new ArrayList();
+    private List<EstadoFinal> estadofinal = new ArrayList();
+    private EstadoInicial estadoincial = new EstadoInicial();
+    private String alfabeto;
+
+    public List<Transicao> getTransition() {
+        return transition;
+    }
+
+    public void setTransition(List<Transicao> transition) {
+        this.transition = transition;
+    }
+
+    public List<Estados> getEstado() {
+        return estado;
+    }
+
+    public void setEstado(List<Estados> estado) {
+        this.estado = estado;
+    }
+
+    public List<EstadoFinal> getEstadofinal() {
+        return estadofinal;
+    }
+
+    public void setEstadofinal(List<EstadoFinal> estadofinal) {
+        this.estadofinal = estadofinal;
+    }
+
+    public EstadoInicial getEstadoincial() {
+        return estadoincial;
+    }
+
+    public void setEstadoincial(EstadoInicial estadoincial) {
+        this.estadoincial = estadoincial;
+    }
+
+    public String getAlfabeto() {
+        return alfabeto;
+    }
+
+    public void setAlfabeto(String alfabeto) {
+        this.alfabeto = alfabeto;
+    }
+}
---
+++ src/pkg2/projeto_tc/Projeto_TC.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg2.projeto_tc;
+
+/**
+ *
+ * @author Victor
+ */
+public class Projeto_TC {
+//IREMOS CRIAR OS METODOS AQUI, CASO NECESSITEM DE OUTRAS CLASSES, É SÓ CRIAR E IMPORTAR AQUI
+ 
+
+    public static void main(String[] args) {
+       
+    }
+    
+}
